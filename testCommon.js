961330f6a93d6b922f458988fddbf6769e2c69e9
shaman2016scratch <g.ilkh@yandex.ru>
2026-07-01 10:59:56 +0300

    Delete test common

--- testCommon.js
+++
@@ -1,9 +0,0 @@
-const DashAttach = require('dashattach');
-
-console.log(await DashAttach.info.users.getRole(7))
-console.log(await DashAttach.info.users.getGradient(7))
-console.log(await DashAttach.info.users.getId(7))
-console.log(await DashAttach.info.users.getUsername(7))
-console.log(await DashAttach.info.projects.getAuthorUsername(100))
-console.log(await DashAttach.featuredProjects(0, 10))
-console.log(await DashAttach.auth.myInfo.isLogin())
