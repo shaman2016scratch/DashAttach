ea11c231dad4c29ad2d8bd84dd693c574910de52
shaman2016scratch <g.ilkh@yandex.ru>
2026-07-01 10:23:23 +0300

    Add tests file

---
+++ test.js
@@ -0,0 +1,9 @@
+import DashAttach from "./index.js";
+
+console.log(await DashAttach.info.users.getRole(7))
+console.log(await DashAttach.info.users.getGradient(7))
+console.log(await DashAttach.info.users.getId(7))
+console.log(await DashAttach.info.users.getUsername(7))
+console.log(await DashAttach.info.projects.getAuthorUsername(100))
+console.log(await DashAttach.featuredProjects())
+console.log(await DashAttach.auth.myInfo.isLogin())
