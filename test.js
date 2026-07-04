98d74cb9bce4c612d64a1ddb50fa86a1dd9e64ff
shaman2016scratch <g.ilkh@yandex.ru>
2026-07-04 13:31:44 +0300

    Update tests

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 import DashAttach from "./index.js";
 
 console.log(await DashAttach.info.users.getRole(7))
-console.log(await DashAttach.info.users.getGradient(7))
 console.log(await DashAttach.info.users.getId(7))
 console.log(await DashAttach.info.users.getUsername(7))
 console.log(await DashAttach.info.projects.getAuthorUsername(100))
-console.log(await DashAttach.featuredProjects(0, 10))
+console.log(await DashAttach.info.projects.getAuthorId(100))
+console.log(await DashAttach.featuredProjects(0, 5))
 console.log(await DashAttach.auth.myInfo.isLogin())
+console.log(await DashAttach.info.users.getGradient(7))
+console.log(await DashAttach.info.projects.stats.fires(100))
