09abcf5028cb65928fd3c41310dce4d2a0c314a3
shaman2016scratch <g.ilkh@yandex.ru>
2026-07-18 13:07:51 +0300

    Update

--- src/help/sb3.js
+++ src/help/sb3.js
@@ -1,20 +1,40 @@
 class SB3 {
-    constructor () {}
+    constructor (project) {
+        this.#project = project
+        this.targets = this.#project.targets
+    }
+
+    get extensionStorage () {
+        return this.#project.extensionStorage
+    }
+
+    target (index) {
+        return this.targets[index]
+    }
 }
 
 class Target {
     constructor (project, number) {
         this.#project = project
         this.#index = number
-        this.targets = this.#project.targets
+        this.#projectData = new SB3(this.#project)
+        this.targets = this.#projectData.targets
     }
 
     get isStage () {
-        return this.targets[this.#number].isStage
+        return this.targets[this.#index].isStage
     }
 
     get name () {
-        return this.targets[this.#number].name
+        return this.targets[this.index].name
+    }
+
+    get vars () {
+        return this.targets[this.#index].variables
+    }
+
+    get lists () {
+        return this.targets[this.#index].lists
     }
 }
 
@@ -24,7 +44,7 @@ class Stage {
     }
 
     get vars () {
-        return this.#stage.variables
+        return this.#stage.vars
     }
 
     get lists () {
